35518216d4b3df73120ea0314ce956383ebabbb8
Zackery Bangs <106060493+zbangs@users.noreply.github.com>
2024-12-18 01:13:16 +0000

    write examples

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'ruby-openai'
+gem 'dotenv'
+gem 'activesupport'
---
+++ chat.rb
@@ -0,0 +1,26 @@
+require 'openai'
+require 'dotenv/load'
+
+client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_API_KEY"))
+
+# Prepare an Array of previous messages
+message_list = [
+  {
+    "role" => "system",
+    "content" => "You are a helpful assistant who talks like Shakespeare."
+  },
+  {
+    "role" => "user",
+    "content" => "Hello! What are the best spots for pizza in Chicago?"
+  }
+]
+
+# Call the API to get the next message from GPT
+api_response = client.chat(
+  parameters: {
+    model: "gpt-3.5-turbo",
+    messages: message_list
+  }
+)
+
+pp api_response
